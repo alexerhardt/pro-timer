5ae4802e923364f4a3e346d629a1cffce659fe38
alexerhardt <alex.erhardt.es@gmail.com>
2019-06-28 11:03:12 +0200

    refactor timer function

--- src/renderer.js
+++ src/renderer.js
@@ -1,37 +1,42 @@
-'use strict';
-const moment = require('moment');
-const momentDurationFormatSetup = require('moment-duration-format');
+"use strict";
+const moment = require("moment");
+require("moment-duration-format");
 
-const $mainView = document.querySelector('.main-view');
-const $settingsView = document.querySelector('.settings-view');
-const $toggleViewButtons = document.querySelectorAll('.toggle-btn');
+const $mainView = document.querySelector(".main-view");
+const $settingsView = document.querySelector(".settings-view");
+const $toggleViewButtons = document.querySelectorAll(".toggle-btn");
 
 const VIEWS = { main: 0, settings: 1 };
 let selectedView = VIEWS.main;
 
-$toggleViewButtons.forEach(($button) => {
-  $button.addEventListener('click', () => {
+$toggleViewButtons.forEach($button => {
+  $button.addEventListener("click", () => {
     if (selectedView === VIEWS.main) {
-      $mainView.classList.add('hidden');
-      $settingsView.classList.remove('hidden');
+      $mainView.classList.add("hidden");
+      $settingsView.classList.remove("hidden");
       selectedView = VIEWS.settings;
     } else {
-      $settingsView.classList.add('hidden');
-      $mainView.classList.remove('hidden');
+      $settingsView.classList.add("hidden");
+      $mainView.classList.remove("hidden");
       selectedView = VIEWS.main;
     }
   });
 });
 
 class Counter {
-  seconds = 0;
-  timer;
+  constructor(renderFunction) {
+    this.startDate = undefined;
+    this.seconds = 0;
+    this.timer = undefined;
+    this.render = renderFunction;
+  }
 
   start() {
     if (!this.timer) {
+      this.startDate = moment.now();
       this.timer = setInterval(() => {
         this.seconds++;
-        this.render();
+        this.render(this.seconds);
       }, 1000);
     }
   }
@@ -47,29 +52,27 @@ class Counter {
       this.stop();
       this.seconds = 0;
       this.timer = undefined;
-      this.render();
+      this.render(this.seconds);
     }
-  }
-
-  render() {
-    const m = moment.duration(this.seconds * 1000).format('HH : mm : ss', {
-      trim: false,
-    });
-    document.querySelector('.counter').innerHTML = m;
   }
 }
 
-let c = new Counter();
-document.querySelector('.start-btn').addEventListener('click', () => {
+function timerRender(seconds) {
+  const m = moment.duration(seconds * 1000).format("HH : mm : ss", {
+    trim: false
+  });
+  document.querySelector(".counter").innerHTML = m;
+}
+
+let c = new Counter(timerRender);
+document.querySelector(".start-btn").addEventListener("click", () => {
   c.start();
 });
 
-document.querySelector('.stop-btn').addEventListener('click', () => {
+document.querySelector(".stop-btn").addEventListener("click", () => {
   c.stop();
 });
 
-document.querySelector('.reset-btn').addEventListener('click', () => {
+document.querySelector(".reset-btn").addEventListener("click", () => {
   c.reset();
 });
-
-
